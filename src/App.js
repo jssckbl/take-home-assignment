ed25ff67faae3c01f82b57482d39543fa8de79af
jssckbl <jessicawoudsma@gmail.com>
2021-06-11 08:42:53 -0500

    added uppercase and lowercase buttons. created two onClick functions that connect via console log

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import './App.css';
-import React from "react";
+import React from 'react';
 
 function App() {
-  const [textInput, setTextInput] = React.useState('Here is some example text.');
+  const [textInput, setTextInput] = React.useState(
+    'Here is some example text.'
+  );
   const [textOutput, setTextOutput] = React.useState('');
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setTextInput(event.target.value);
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setTextOutput('Your formatted text will go here!')
+    setTextOutput('Your formatted text will go here!');
   };
 
+  function handleUpperCase() {
+    console.log('uppercase clicked');
+  }
+
+  function handleLowerCase() {
+    console.log('lowercase clicked');
+  }
+
+  // Add two functions- handleUpperCase and handleLowerCase.
+  // Connect the two functions to the uppercase and lowercase buttons.
+
   return (
-    <div className="App">
+    <div className='App'>
       <header>
         <h1>Career Lab | Take-Home Assignment</h1>
       </header>
       <form onSubmit={handleSubmit}>
         <label>
-          <textarea onChange={handleChange} value={textInput}/>
+          <textarea onChange={handleChange} value={textInput} />
         </label>
-        <input type="submit" value="Submit"/>
+        {/* <input type='submit' value='Submit' /> */}
+        <button onClick={handleUpperCase}>UPPERCASE</button>
+        <button onClick={handleLowerCase}>lowercase</button>
       </form>
-      <div id="result">
-        {textOutput}
-      </div>
+      <div id='result'>{textOutput}</div>
     </div>
   );
 }
